f7e4d401d36fbf6108942c346e7644879dfc7d99
Losev Vladyslav <135152001+Obbjh@users.noreply.github.com>
2023-12-08 17:47:22 +0200

    in progress

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items';  
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery")
+
+const galleryCurrent = galleryItems.map(({preview, description, original}) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+  `).join('');
+
+galleryList.insertAdjacentHTML('afterbegin', galleryCurrent);
+  
+//Використання simplelightbox
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,6 @@
+import Player from '@vimeo/player';
 
+
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
